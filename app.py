6a3c54af55b667fb5ca33a933cd1d7e3d75d8c58
razgonyaevm <razgonyaevm@gmail.com>
2022-03-19 21:41:30 +0500

    correct

--- app.py
+++ app.py
@@ -102,7 +102,6 @@ def handle_dialog(req, res, text):
         # Пользователь согласился, прощаемся.
         if text == 'слона':
             res['response']['text'] = f'Слона можно найти на Яндекс.Маркете!'
-            req['session']['new'] = True
         else:
             res['response']['text'] = 'Кролика можно найти на Яндекс.Маркете!'
             res['response']['end_session'] = True
